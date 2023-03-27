d0cbfdbc62804f9def6389504bc7ba8ae53e9c02
张泽清 <1804924578@qq.com>
2023-03-27 16:52:10 +0800

    纯java

--- src/main/java/com/example/ex3_2_back/config/MyAlgorithmConfiguration.java
+++ src/main/java/com/example/ex3_2_back/config/MyAlgorithmConfiguration.java
@@ -1,16 +1,14 @@
 package com.example.ex3_2_back.config;
 
-import com.example.ex3_2_back.util.TestAlgorithm;
+import com.example.ex3_2_back.algorithm.MyAlgorithm;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyAlgorithmConfiguration {
     @Bean
-    public TestAlgorithm getTestAlgorithm() {
-        return new TestAlgorithm();
+    public MyAlgorithm getTestAlgorithm() {
+        return new MyAlgorithm();
     }
-
-
 
 }
