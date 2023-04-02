63ba76eefbf90697a53b18808df40d383f7cc471
张泽清 <1804924578@qq.com>
2023-04-02 11:26:27 +0800

    Movie

--- src/main/java/com/example/ex3_2_back/repository/MovieRepository.java
+++ src/main/java/com/example/ex3_2_back/repository/MovieRepository.java
@@ -3,5 +3,11 @@ package com.example.ex3_2_back.repository;
 import com.example.ex3_2_back.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+    List<Movie> findByOriginalTitle(String originalTitle);
+
+    Optional<Movie> findById(Integer id);
 }
