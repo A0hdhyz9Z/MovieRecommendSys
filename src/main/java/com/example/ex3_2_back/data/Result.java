87b861662ff9981a001b2a80ffc179facaa54a31
ZhangzeqingCN <1804924578@qq.com>
2023-05-27 22:48:50 +0800

    Authentic Interceptor enhance

--- src/main/java/com/example/ex3_2_back/data/Result.java
+++ src/main/java/com/example/ex3_2_back/data/Result.java
@@ -2,17 +2,44 @@ package com.example.ex3_2_back.data;
 
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Builder
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Result<Data> {
-    boolean success;
-    String message;
-    Data data;
+@ToString
+@EqualsAndHashCode
+public class Result {
+    private boolean success;
+    private int code;
+    private String message;
+    private Object data;
+    public final List<Object> errors = new ArrayList<>();
+    public final List<Object> devMessages = new ArrayList<>();
 
-    public Result(boolean b, String message) {
-        this(b, message, null);
+    public static Result success() {
+        return Result.builder().success(true).build();
     }
+
+    public static Result success(Object data) {
+        return Result.builder().success(true).data(data).build();
+    }
+
+    public static Result error(String message) {
+        return Result.builder().success(false).message(message).build();
+    }
+
+    public Result addErrors(Object error) {
+        errors.add(error);
+        return this;
+    }
+
+    public Result addDevMessages(Object devMessage) {
+        devMessages.add(devMessage);
+        return this;
+    }
+
 }
--- src/main/java/com/example/ex3_2_back/interceptor/AuthInterceptor.java
+++ src/main/java/com/example/ex3_2_back/interceptor/AuthInterceptor.java
@@ -1,6 +1,7 @@
 package com.example.ex3_2_back.interceptor;
 
 
+import com.example.ex3_2_back.data.Result;
 import com.example.ex3_2_back.entity.User;
 import com.example.ex3_2_back.security.MySecurity;
 import jakarta.annotation.Resource;
@@ -8,16 +9,14 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.event.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Random;
+import java.util.*;
 
 @Slf4j
 @Component
@@ -58,6 +57,59 @@ public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 
+    }
+
+    /**
+     * 检查token的有效性并更新token
+     *
+     * @param request  当次的请求
+     * @param response 当次的响应
+     * @return 结果
+     */
+    @NotNull
+    public Result validateAndUpdateToken(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response) {
+        var cookies = request.getCookies();
+
+        if (cookies == null || cookies.length == 0) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            var message = String.format("%s: No token in cookie", request.getRequestURL());
+            log.info(message);
+            return Result.error(message);
+        }
+
+        Optional<Cookie> optionalCookie = Arrays.stream(cookies).filter(cookie -> cookie.getName().equals("token")).findAny();
+        String token = optionalCookie.map(Cookie::getValue).orElse("");
+
+        log.info(String.format("old token: %s", token));
+        var optionalUser = mySecurity.decToken(token);
+
+        if (optionalUser.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            var message = String.format("%s: No username in token", request.getRequestURL());
+            log.info(message);
+            return Result.error(message);
+        }
+
+        var username = optionalUser.get().getName();
+        request.setAttribute("username", username);
+        setTokenCookie(username, response);
+
+        return Result.success();
+    }
+
+    /**
+     * 设置token
+     *
+     * @param username 用户名
+     * @param response 当次的响应
+     */
+    public void setTokenCookie(String username, @NotNull HttpServletResponse response) {
+        String token = mySecurity.genToken(User.builder().name(username).build());
+        log.info(String.format("new token for %s: %s", username, token));
+        var cookie = new Cookie("token", token);
+        cookie.setPath("/");
+        cookie.setMaxAge(30000000);
+        response.addCookie(cookie);
     }
 }
 
