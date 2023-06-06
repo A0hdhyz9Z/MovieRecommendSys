3c4ed0fdee5233f96ef087f12736c5873adce2b4
陈增耀 <2692830800@qq.com>
2023-06-06 10:42:51 +0800

    czy-添加实体Recommend

---
+++ src/main/java/com/example/ex3_2_back/entity/Recommend.java
@@ -0,0 +1,30 @@
+package com.example.ex3_2_back.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "recommendation")
+public class Recommend {
+    @Id
+    @Column(name = "recommend_id")
+    // 加上自动生成
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    User user;
+    @OneToOne
+    @JoinColumn(name = "movie_id")
+    Movie movie;
+    @OneToOne
+    @JoinColumn(name = "score")
+    Float score;
+
+}
