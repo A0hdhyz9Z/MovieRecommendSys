89b4ab5f19dcdb0963a226d2cd0d88b3540fde32
陈增耀 <2692830800@qq.com>
2023-06-08 23:32:36 +0800

    czy-添加评分

--- src/main/java/com/example/ex3_2_back/controller/RateController.java
+++ src/main/java/com/example/ex3_2_back/controller/RateController.java
@@ -1,9 +1,12 @@
 package com.example.ex3_2_back.controller;
 
 import com.example.ex3_2_back.domain.Result;
+import com.example.ex3_2_back.entity.Favorite;
+import com.example.ex3_2_back.entity.Movie;
 import com.example.ex3_2_back.entity.Rate;
 import com.example.ex3_2_back.entity.User;
 import com.example.ex3_2_back.repository.RateRepository;
+import com.example.ex3_2_back.repository.UserRepository;
 import com.example.ex3_2_back.service.RateService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -16,16 +19,17 @@ import org.springframework.web.bind.annotation.*;
 public class RateController {
 
     RateRepository rateRepository;
-    RateService rateService;
-
-    @Autowired
-    public void setRateService(RateService rateService) {
-        this.rateService = rateService;
-    }
 
     @Autowired
     public void setRateRepository(RateRepository rateRepository) {
         this.rateRepository = rateRepository;
+    }
+
+    UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/{id}")
@@ -40,10 +44,20 @@ public class RateController {
         return Result.success(rateRepository.findAllByUser(User.builder().name(username).build()));
     }
 
-    //    TO:DO bug待修改
-    @PostMapping
-    public Result create(@RequestBody Rate rate) {
-        return Result.success(rateRepository.save(rate));
+    @PostMapping("/{movieId}")
+    public Result create(HttpServletRequest request,@RequestBody Rate rate, @PathVariable Integer movieId) {
+        String username = request.getHeader("username");
+        var optUser=userRepository.findByName(username);
+        if (optUser.isEmpty()) {
+            return Result.error("用户不存在");
+        }
+
+        User user = optUser.get();
+
+        return Result.success(Rate.builder()
+                .user(user)
+                .movie(Movie.builder().id(movieId).build())
+                .build());
     }
 
 
