92e7077b53b7e7653d54d2fb2a7a92b2472a47c6
Your Name <you@example.com>
2023-06-10 22:16:28 +0800

    添加了repository中的Operation&RestResource

--- src/main/java/com/example/ex3_2_back/controller/RateController.java
+++ src/main/java/com/example/ex3_2_back/controller/RateController.java
@@ -9,6 +9,7 @@ import com.example.ex3_2_back.entity.User;
 import com.example.ex3_2_back.repository.MovieRepository;
 import com.example.ex3_2_back.repository.RateRepository;
 import com.example.ex3_2_back.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,7 +25,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/rate")
 @Slf4j
-@Tag(name = "RateController")
+@Tag(name = "RateController", description = "RateController")
 public class RateController {
 
     RateRepository rateRepository;
@@ -34,36 +35,43 @@ public class RateController {
     MovieRepository movieRepository;
 
     @Autowired
+    @Operation(summary = "setMovieRepository", description = "setMovieRepository")
     public void setMovieRepository(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
     }
 
     @Autowired
+    @Operation(summary = "setUserRepository", description = "setUserRepository")
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Autowired
+    @Operation(summary = "setRateRepository",description = "setRateRepository")
     public void setRateRepository(RateRepository rateRepository) {
         this.rateRepository = rateRepository;
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "单个ID查询评分",description = "单个ID查询评分")
     public TResult<Optional<Rate>> findOne(@PathVariable Integer id) {
         return TResult.success(rateRepository.findById(id));
     }
 
     @GetMapping
+    @Operation(summary = "查询所有用户评分",description = "查询所有用户评分")
     public TResult<List<Rate>> findAllOfUser(@RequestHeader("username") String username) {
         return TResult.success(rateRepository.findAllByUser(User.builder().name(username).build()));
     }
 
     //    TO:DO bug待修改
     @PostMapping("/{movieId}")
+    @Operation(summary = "用户进行评分",description = "用户进行评分")
     public Result postRate(
             @Schema(defaultValue = "15") @RequestHeader("username") String username,
             @Schema(defaultValue = "862") @PathVariable Integer movieId,
-            @NotNull @RequestBody PostRateDomain postRateDomain) {
+            @NotNull @RequestBody PostRateDomain postRateDomain
+    ) {
 
         Optional<User> optionalUser = userRepository.findByName(username);
         if (optionalUser.isEmpty()) {
--- src/main/java/com/example/ex3_2_back/controller/WorkerController.java
+++ src/main/java/com/example/ex3_2_back/controller/WorkerController.java
@@ -2,6 +2,7 @@ package com.example.ex3_2_back.controller;
 
 import com.example.ex3_2_back.domain.Result;
 import com.example.ex3_2_back.repository.WorkerRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ public class WorkerController {
     WorkerRepository workerRepository;
 
     @Autowired
+    @Operation(summary = "setWorkerRepository", description = "setWorkerRepository")
     public void setWorkerRepository(WorkerRepository workerRepository) {
         this.workerRepository = workerRepository;
     }
